7b8f86c4d4e2c92eeb0c5a5262f0efd462de8ef7
Rene Mercadel <renemercadel@gmail.com>
2022-09-17 18:41:43 -0500

    Implementing conditional navbar options user authentication and simple css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_230216) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_17_231638) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "start"
     t.datetime "end"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_230216) do
     t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
